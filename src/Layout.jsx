ae5144a863e53b7376bea0640afbf3ff59ace4de
DJTHEMAN <shawndunsmorejr2@gmail.com>
2026-02-28 22:37:00 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { base44 } from '@/api/base44Client';
 import LiveChat from './components/game/LiveChat';
+import ProfileModal from './components/game/ProfileModal';
 import {
   Box, Swords, Coins, TrendingUp, Gift, Award, Users,
   Menu, X, Home, LogOut, Zap, ChevronLeft,
