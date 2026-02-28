5e876f728f33704642014786cc1402b97250520c
DJTHEMAN <shawndunsmorejr2@gmail.com>
2026-02-28 22:21:50 +0000

    File changes

--- src/components/game/BattleArena.jsx
+++ src/components/game/BattleArena.jsx
@@ -191,7 +191,7 @@ function PlayerColumn({ player, playerColor, isWinner, wonItems, spinPhase, case
         }
 
         return (
-          <div className="px-2 pb-2 flex-shrink-0">
+          <div className="px-2 pb-2 flex-1 min-h-0">
             {spinPhase === 'magic_spin' && (
               <div className="text-center mb-1">
                 <span className="text-[10px] font-bold text-cyan-300 tracking-widest uppercase">✦ Magic Spin ✦</span>
